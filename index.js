fc635c7a2914fa9961138b336013ff14901cf958
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-10-17 18:09:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function getMarkdown() {
-  var md = fetch("/README.md")
-  return md
+  var md = fetch("/README.md");
+  return md;
 }
 let converter = new showdown.Converter();
 getMarkdown()
   .then((response) => response.text())
   .then((data) => {
-    console.log(data)
+    
     let html = converter.makeHtml(data);
-    console.log(html)
+    let cont = document.getElementById("readme").firstChild;
+    cont.innerHTML = html;
 });
