57460ac1be97199b5587eb58db6b09b4ac86d91f
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-10-17 17:19:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+let text = fetch("/README.md")
+  .then((response) => response.text)
+  .then(((data) => return data);
 
+let converter = new showdown.Converter(),
+    html      = converter.makeHtml(text);
