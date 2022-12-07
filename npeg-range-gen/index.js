ed395e27e0a4e98c7a6cd1e8b4be97a8d9b05f53
thatrandomperson5 <thatrandomperson5@users.noreply.github.com>
2022-12-07 18:11:50 +0000

    :art: Format code with prettier

--- npeg-range-gen/index.js
+++ npeg-range-gen/index.js
@@ -1,75 +1,75 @@
 // [["a", "Z"], "?"]!["B"]
 
 const range = (start, stop, step = 1) =>
-  Array(Math.ceil((stop - start) / step)).fill(start).map((x, y) => x + y * step)
+  Array(Math.ceil((stop - start) / step))
+    .fill(start)
+    .map((x, y) => x + y * step);
 
 function allChars(lst) {
   var output = [];
   for (var i in lst) {
     let item = lst[i];
     if (item instanceof Array) {
-      let itemNum = [item[0].codePointAt(0), item[1].codePointAt(0)].sort((a, b) => a - b);
+      let itemNum = [item[0].codePointAt(0), item[1].codePointAt(0)].sort(
+        (a, b) => a - b
+      );
 
-
-      let irange = range(itemNum[0], itemNum[1] + 1)
+      let irange = range(itemNum[0], itemNum[1] + 1);
 
       for (let x in irange) {
-        let item = irange[x]
-        output.push(item) //String.fromCharCode
+        let item = irange[x];
+        output.push(item); //String.fromCharCode
       }
-
     } else {
-      output.push(item.codePointAt(0))
+      output.push(item.codePointAt(0));
     }
   }
-  return output
+  return output;
 }
 
 function makeRange(nums) {
-  let len = nums.length
+  let len = nums.length;
   nums.push(0);
-  var output = "{"
-  var tail = ""
-  var last = null
+  var output = "{";
+  var tail = "";
+  var last = null;
   for (let i = 0; i < len; i++) {
     let item = nums[i];
-    let lookahead = nums[i + 1]
+    let lookahead = nums[i + 1];
     // console.log(item, lookahead, String.fromCharCode(item))
     if (item + 1 === lookahead) {
       if (last == null) {
-        last = String.fromCharCode(item)
+        last = String.fromCharCode(item);
       } else {
-        continue
+        continue;
       }
     } else {
       if (last != null) {
-        output += `'${last}'..'${String.fromCharCode(item)}', `
-        last = null
+        output += `'${last}'..'${String.fromCharCode(item)}', `;
+        last = null;
       } else {
-        tail += `'${String.fromCharCode(item)}', `
+        tail += `'${String.fromCharCode(item)}', `;
       }
     }
   }
-  output += (tail + "}")
-  return output
+  output += tail + "}";
+  return output;
 }
 
 function convert(event) {
-  let val = document.getElementById("input").value
+  let val = document.getElementById("input").value;
   //console.log(val[0].codePointAt(0));
-  var use = JSON.parse(val.split("!")[0])
-  var not = JSON.parse(val.split("!")[1])
-  use = allChars(use)
-  not = allChars(not)
+  var use = JSON.parse(val.split("!")[0]);
+  var not = JSON.parse(val.split("!")[1]);
+  use = allChars(use);
+  not = allChars(not);
   for (let i in not) {
-    let item = not[i]
+    let item = not[i];
     const index = use.indexOf(item);
     if (index > -1) {
       use.splice(index, 1);
     }
-
   }
   use = use.sort((a, b) => a - b);
-  document.getElementById("output").textContent = makeRange(use)
-
+  document.getElementById("output").textContent = makeRange(use);
 }
